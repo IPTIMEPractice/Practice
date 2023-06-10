0c5fa7066b587a98aa54da5d61f8e6665fe3c22b
Seongeun Lee <tjsrms1227@gmail.com>
2023-06-10 19:07:03 +0900

    초기 설정 수정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React from 'react'
+
 function App() {
-  return <div>
-    <p>아이피타임 깃플로우 연습</p>
-    <p>연습마니 살기리다</p>
-    <p>연습벌레</p>
-
-
-    </div>;
+    return (
+        <div>
+            <p>아이피타임 깃플로우 연습</p>
+            <p>연습마니 살기리다</p>
+            <p>연습벌레</p>
+        </div>
+    )
 }
-export default App;
+export default App
