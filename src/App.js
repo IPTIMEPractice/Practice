229fc231fab17b95efd3401c491070f29a445a43
eunchokang <wkdb0425@gmail.com>
2023-06-13 11:44:35 +0900

    Feat: tab-menu 구현
    - 공통컴포넌트 중 tab-menu구현

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React from 'react'
-import MButton from './components/common/Button/MButton.jsx'
-import LButton from './components/common/Button/LButton.jsx'
-import MsButton from './components/common/Button/MsButton.jsx'
-import SButton from './components/common/Button/SButton.jsx'
 
 function App() {
-  return (
-    <div>
-      <LButton />
-      <MButton />
-      <MsButton />
-      <SButton />
-    </div>
-  )
+
+    return (
+        <div></div>
+    )
 }
 export default App
