b514838d96691b409f328070a641d8fe0d183236
suminson <sabi1111@nate.com>
2023-06-13 10:31:38 +0900

    Style: 수직정렬 수정, reset값 삭제

--- src/components/common/Top/TopChatNav.jsx
+++ src/components/common/Top/TopChatNav.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import styled from 'styled-components'
-import arrowLeft from '../../../assets/img/icon-arrow-left.svg'
-import optionImg from '../../../assets/img/icon-more-vertical.svg'
+import React from 'react';
+import styled from 'styled-components';
+import arrowLeft from '../../../assets/img/icon-arrow-left.svg';
+import optionImg from '../../../assets/img/icon-more-vertical.svg';
 
 const TopDiv = styled.div`
   width: 39rem;
@@ -11,29 +11,25 @@ const TopDiv = styled.div`
   justify-content: flex-start;
   align-items: center;
   padding: 0 1.6rem;
-`
+  gap: 1rem;
+`;
 const ArrowLeftBtn = styled.button`
-  background-color: inherit;
-  border: none;
   width: 2.2rem;
   height: 2.2rem;
   background-image: url(${arrowLeft});
-`
+`;
 const MainTitle = styled.h1`
-  font-family: 'Spoqa Han Sans Neo';
-  font-style: normal;
   font-weight: 500;
   font-size: 18px;
   line-height: 22px;
-`
+`;
 
 const OptionBtn = styled.button`
-  background-color: inherit;
-  border: none;
   width: 2.4rem;
   height: 2.4rem;
   background-image: url(${optionImg});
-`
+  margin-left: auto;
+`;
 export default function TopChatNav() {
   return (
     <TopDiv>
@@ -41,5 +37,5 @@ export default function TopChatNav() {
       <MainTitle>채팅 타이틀</MainTitle>
       <OptionBtn />
     </TopDiv>
-  )
+  );
 }
--- src/components/common/Top/TopMainNav.jsx
+++ src/components/common/Top/TopMainNav.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import styled from 'styled-components'
-import searchImg from '../../../assets/img/icon-search.svg'
+import React from 'react';
+import styled from 'styled-components';
+import searchImg from '../../../assets/img/icon-search.svg';
 
 const TopDiv = styled.div`
   width: 39rem;
@@ -8,27 +8,24 @@ const TopDiv = styled.div`
   display: flex;
   box-sizing: border-box;
   justify-content: space-between;
-  padding: 1.3rem 1.6rem;
-`
+  align-items: center;
+  padding: 0 1.6rem;
+`;
 const MainTitle = styled.h1`
-  font-family: 'Spoqa Han Sans Neo';
-  font-style: normal;
   font-weight: 500;
   font-size: 18px;
   line-height: 22px;
-`
+`;
 const SearchBtn = styled.button`
-  background-color: inherit;
-  border: none;
   width: 2.4rem;
   height: 2.4rem;
   background-image: url(${searchImg});
-`
+`;
 export default function TopMainNav() {
   return (
     <TopDiv>
       <MainTitle>타이틀명 피드</MainTitle>
       <SearchBtn />
     </TopDiv>
-  )
+  );
 }
