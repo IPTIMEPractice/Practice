bf00145907540582265cb18413d4af1658c974b8
eunchokang <wkdb0425@gmail.com>
2023-06-13 15:16:39 +0900

    Fix: input type 추가
    - App.js 괄호 짝 맞추기
    - input type="text" 추가

--- src/components/common/Comment/Comment.jsx
+++ src/components/common/Comment/Comment.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import basicProfile from '../../../../src/assets/img/basic-profile.svg';
-import { CommentDiv, Commentinp, CommentBtn } from './Comment.styled';
+import { CommentDiv, CommentInp, CommentBtn } from './Comment.styled';
 
 export default function Comment() {
   return (
     <CommentDiv>
       <img src={basicProfile} />
-      <Commentinp placeholder="댓글 입력하기..." />
+      <CommentInp type="text" placeholder="댓글 입력하기..." />
       <CommentBtn>게시</CommentBtn>
     </CommentDiv>
   );
